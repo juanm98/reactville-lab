c10d768adeea0fa5d8f0a52d18cdec1d30ebde55
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-27 06:53:13 -0400

    Complete Step 5

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,10 +1,14 @@
 // Components & Assets
 import Logo from '../../assets/react-logo.png'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <nav className="navigation-bar">
-
+          <NavLink id='logo' to='/'>
+      <img src={Logo} alt='react-logo'/>
+    </NavLink>
+    <NavLink to='/burgers'>BURGER SHOP</NavLink>
     </nav>
   )
 }
